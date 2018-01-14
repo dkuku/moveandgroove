5b8aeaf621fa84f5626037ac7d33f16e84558fbe
Daniel Kukula <daniel.kuku@gmail.com>
2018-01-14 14:37:48 -0500

    initial data for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+
+user =  User.new(
+  email: "daniel@wp.pl",
+    name: "Daniel",
+    password: "123456",
+    password_confirmation: "123456"
+  )
+user.save!
+
+puts "1 users created"
+
+
+
+10.times do |activity_list|
+  ActivityList.create!(
+  name: "running",
+  theme: "default",
+  main_image: "http://via.placeholder.com/400x400",
+  thumb_image: "http://via.placeholder.com/50x50"
+  )
+end
+
+puts "10 activity_list created"
+
+
+10.times do |activity|
+  Activity.create!(
+    activity_list_id: "1",
+    date: "1/1/2018".to_date , 
+    duration: "15", 
+    kcal: "200",
+    user_id: "1"
+  )
+end
+
+puts "10 activities create"
